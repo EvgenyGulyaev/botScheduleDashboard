378dc1dfaa25822a6b2a9a60a634bd1415c4f788
Евгений Гуляев <wardercompany@gmail.com>
2025-12-20 21:39:00 +0300

    Update block User

--- internal/event/consumer/user.go
+++ internal/event/consumer/user.go
@@ -9,11 +9,13 @@ type User struct {
 	Id   int64  `json:"id"`
 	Name string `json:"username"`
 	Net  string `json:"network"`
+	Text string `json:"text"`
 }
 
 func HandleUser(u User) {
 	r := store.GetSocialUserRepository()
 	_, err := r.CreateSocialUser(u.Id, u.Net, u.Name)
+	// TODO text кладем текст для отображения в будущем в истории, чтобы общаться при блокировке
 	if err != nil {
 		log.Println(err)
 	}
